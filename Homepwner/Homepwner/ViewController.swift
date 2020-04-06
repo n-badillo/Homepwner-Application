f553794a55c6df19918725a672de2789923a2ae6
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-05 20:37:08 -0700

    10. Initial Commit

---
+++ Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by CSUFTitan on 4/5/20.
+//  Copyright © 2020 Nancy Badillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
