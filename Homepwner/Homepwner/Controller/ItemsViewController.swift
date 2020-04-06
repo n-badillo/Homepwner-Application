559ababa5c390db96e172b6acc65b26f745bfe3c
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-05 20:44:54 -0700

    10.  Setup UITableViewController

---
+++ Homepwner/Homepwner/Controller/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by CSUFTitan on 4/5/20.
+//  Copyright © 2020 Nancy Badillo. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController{
+    
+}
