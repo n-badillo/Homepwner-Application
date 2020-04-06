99746cfabfac1d923eb0b8117c7cf7d44ac4dff9
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-06 02:22:47 -0700

    12. Subclass UITableViewCell

---
+++ Homepwner/Homepwner/View/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by CSUFTitan on 4/6/20.
+//  Copyright © 2020 Nancy Badillo. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell{
+    
+    
+}
