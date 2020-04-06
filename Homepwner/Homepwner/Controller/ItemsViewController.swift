6417191498da2f4cd3fdd0c9bddbb9af71845200
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-06 04:50:09 -0700

    14. Add "Appearing and Disappearing Views"

--- Homepwner/Homepwner/Controller/ItemsViewController.swift
+++ Homepwner/Homepwner/Controller/ItemsViewController.swift
@@ -69,6 +69,12 @@ class ItemsViewController: UITableViewController{
         tableView.estimatedRowHeight = 65
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
