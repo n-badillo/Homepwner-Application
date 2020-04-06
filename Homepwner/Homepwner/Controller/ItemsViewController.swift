175eaff16e80de4b6107b3ee02efe90fa38a01b1
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-06 04:37:38 -0700

    13.  Pass Data from ItemsVC to DetailVC

--- Homepwner/Homepwner/Controller/ItemsViewController.swift
+++ Homepwner/Homepwner/Controller/ItemsViewController.swift
@@ -38,6 +38,24 @@ class ItemsViewController: UITableViewController{
         }
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        // If the triggered segue is the "showItem" segue ...
+        switch segue.identifier{
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row{
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -108,4 +126,7 @@ class ItemsViewController: UITableViewController{
         // Update the model
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
+    
+    
+    
 }
