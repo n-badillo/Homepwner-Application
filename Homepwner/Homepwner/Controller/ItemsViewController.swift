fc1f1508591d6c4d3fc4091636b2290f2fd2609d
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-05 21:46:03 -0700

    10. Add Content Insets

--- Homepwner/Homepwner/Controller/ItemsViewController.swift
+++ Homepwner/Homepwner/Controller/ItemsViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class ItemsViewController: UITableViewController{
     var itemStore: ItemStore!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
