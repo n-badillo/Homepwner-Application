3ec4e9c1a0e25bc5dc24fae25b02dae224a74165
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-06 02:28:14 -0700

    12.  Expose the Properties of ItemCell

--- Homepwner/Homepwner/View/ItemCell.swift
+++ Homepwner/Homepwner/View/ItemCell.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ItemCell: UITableViewCell{
-    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
     
 }
