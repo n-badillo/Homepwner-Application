4a5965b6c3858912f9660699315cb7df1f74b950
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-06 22:04:04 -0700

    17.  Add Size Classes

--- Homepwner/Homepwner/Model/ImageStore.swift
+++ Homepwner/Homepwner/Model/ImageStore.swift
@@ -43,7 +43,11 @@ class ImageStore{
         cache.removeObject(forKey: key as NSString)
         
         let url = imageURL(forKey: key)
-        FileManager.default.removeItem(at: url)
+        do{
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
     }
     
     func imageURL(forKey key: String) -> URL {
